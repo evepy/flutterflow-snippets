d265671754c90a2f2404458093ceddb633bdcecd
Evelyn Barrantes <85376092+evepy@users.noreply.github.com>
2024-09-30 00:22:02 +0000

    week-set

---
+++ customFunctions/greetings.dart
@@ -0,0 +1,30 @@
+//Returns a greeting according to the user's time zone
+import 'dart:convert';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import '/flutter_flow/lat_lng.dart';
+import '/flutter_flow/place.dart';
+import '/flutter_flow/uploaded_file.dart';
+import '/flutter_flow/custom_functions.dart';
+import '/backend/backend.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '/backend/schema/structs/index.dart';
+import '/auth/firebase_auth/auth_util.dart';
+
+String greeting(DateTime timestamp) {
+  // return "morning" if it is morning, "afternoon" if afternoon and "night" if it is night
+  var hour = timestamp.hour;
+  if (hour >= 0 && hour < 12) {
+    return "Good morning,";
+  } else if (hour >= 12 && hour < 17) {
+    return "Good afternoon,";
+  } else {
+    return "Goodnight,";
+  }
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
